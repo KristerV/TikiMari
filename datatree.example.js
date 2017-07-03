debc09dd880760aea7909d2a33718465a2872ddc
Krister Viirsaar <krister.viirsaar@gmail.com>
2017-07-03 21:29:40 +0300

    Create datatree.example.js

---
+++ datatree.example.js
@@ -0,0 +1,34 @@
+data = {
+	designtree: [
+		{
+			id: "m",
+			name: "Mees",
+			children: ["r", "asi"],
+			category: "shirt",
+			price: 0,
+			svgURL: "https://drive.google.com/Tikimari/stuff",
+			photoURL: "https://drive.google.com/Tikimari/stuff",
+			textures: [6, 34, 2]
+		},
+		{
+			id: "asi"
+		},
+		{
+			id: "r"
+		},
+	],
+	textures: [
+		{
+			id: 6,
+			name: "Velvet",
+			variantsURLs: ["http://url.com/tex1.jpg"]
+		}
+	],
+	gallery: [
+		{
+			src: "",
+			tags: [],
+			designURL: "",
+		}
+	]
+}
